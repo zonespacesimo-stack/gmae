174150621055472870ecc3940d9862a98b0a58ce
zonespacesimo-stack <zonespace.simo@gmail.com>
2026-08-06 00:37:15 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+/**
+ * script.js - Single entry point for Vite bundling
+ * Imports all game modules in correct order
+ */
+
+// Import all modules in dependency order
+import './data.js';
+import './effects.js';
+import './levels.js';
+import './ui.js';
+import './game.js';
+import './main.js';
